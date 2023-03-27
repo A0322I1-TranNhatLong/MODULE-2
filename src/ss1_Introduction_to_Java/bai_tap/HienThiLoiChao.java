0de1c0e5a90a124fe4d5586e2c52ab46cd755072
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2023-03-27 18:44:41 +0700

    upload

--- src/ss1_Introduction_to_Java/bai_tap/HienThiLoiChao.java
+++ src/ss1_Introduction_to_Java/bai_tap/HienThiLoiChao.java
@@ -2,7 +2,7 @@ package ss1_Introduction_to_Java.bai_tap;
 
 import java.util.Scanner;
 
-public class hien_thi_loi_chao {
+public class HienThiLoiChao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhập tên của bạn : ");
---
+++ src/ss1_Introduction_to_Java/bai_tap/UngDungChuyenDoiTienTe.java
@@ -0,0 +1,15 @@
+package ss1_Introduction_to_Java.bai_tap;
+
+import java.util.Scanner;
+
+public class UngDungChuyenDoiTienTe {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Mời bạn nhập số tiền USD : ");
+        usd = sc.nextDouble();
+        double quydoi = usd * vnd;
+        System.out.print("Giá trị VNĐ : " + quydoi);
+    }
+}
