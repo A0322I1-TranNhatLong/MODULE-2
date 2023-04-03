4429e3b9a9b895fd4df63af3c7bdcda7c513d41e
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2023-04-03 18:45:31 +0700

    upload

---
+++ src/ss4_opp/bai_tap/XayDungLopStopWatch/Main.java
@@ -0,0 +1,20 @@
+package ss4_opp.bai_tap.XayDungLopStopWatch;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        StopWatch watch1 = new StopWatch();
+        watch1.start();
+        int[] array = new int[100000];
+        for (int i = 0; i < array.length; i++) {
+            Random rand = new Random();
+            array[i] = rand.nextInt();
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        watch1.stop();
+        System.out.println("Thời gian thực thi:" + watch1.getElapsedTime() + " ms");
+    }
+}
---
+++ src/ss4_opp/bai_tap/XayDungLopStopWatch/StopWatch.java
@@ -0,0 +1,32 @@
+package ss4_opp.bai_tap.XayDungLopStopWatch;
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void start() {
+        long time1 = System.currentTimeMillis();
+        this.startTime = time1;
+    }
+
+    public void stop() {
+        long time2 = System.currentTimeMillis();
+        this.endTime = time2;
+    }
+
+    public long getElapsedTime() {
+        return endTime - startTime;
+    }
+}
