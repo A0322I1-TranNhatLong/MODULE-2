323186ba7cb039e5896063dc3b83b5d5a77dd1a7
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2023-04-09 03:30:29 +0700

    upload

---
+++ src/ss6_ke_thua/bai_tap/PointMoveablePoint/TestPoint.java
@@ -0,0 +1,8 @@
+package ss6_ke_thua.bai_tap.PointMoveablePoint;
+
+public class TestPoint {
+    public static void main(String[] args) {
+        MoveablePoint moveablePoint = new MoveablePoint(3, 9, 4, 30);
+        System.out.println(moveablePoint.toString());
+    }
+}
