d1ab968a8b8ba755d4b4fcbc4ea3f65bdc7a412c
A0322I1-TranNhatLong <trannhatlong2001@gmail.com>
2023-05-10 17:56:34 +0700

    upload

---
+++ src/ss15_xu_li_ngoai_le/bai_tap/IllegalTriangleException.java
@@ -0,0 +1,9 @@
+package ss15_xu_li_ngoai_le.bai_tap;
+
+import java.util.Scanner;
+
+class IllegalTriangleException extends Exception {
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
+}
